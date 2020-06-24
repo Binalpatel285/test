df6a4878b6212a6081da6eb704bf30a523f419f5
Binal Patel <binalpatel285@gmail.com>
2020-06-24 21:25:26 +0530

    hey i removed the spcae from line 6

--- count.js
+++ count.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req,  res){
- res.send('this is homepage');
+    res.send('this is homepage');
 });
 
 app.get("/example",function(req,res){
